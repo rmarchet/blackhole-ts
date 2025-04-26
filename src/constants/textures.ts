7b735459b7deb9849c8e89e69cf212fe6438ed9e
Roberto Marchetti <marchetti@beefree.io>
2025-04-26 17:38:16 +0200

    Added more textures

--- src/constants/textures.ts
+++ src/constants/textures.ts
@@ -7,6 +7,8 @@ import diskArrowsUrl from '../assets/accretion_disk_arrows.png';
 import diskYellowUrl from '../assets/accretion_disk_yellow.png';
 import diskBlueUrl from '../assets/accretion_disk_blue.png';
 import diskGridUrl from '../assets/accretion_disk_grid.png';
+import diskChaoticUrl from '../assets/accretion_disk_chaotic.png';
+import diskThermalUrl from '../assets/accretion_disk_thermal.png';
 
 export const IMAGES = {
   starUrl,
@@ -18,6 +20,8 @@ export const IMAGES = {
   diskYellowUrl,
   diskBlueUrl,
   diskGridUrl,
+  diskChaoticUrl,
+  diskThermalUrl,
 }
 
 export const DISK_TEXTURES = {
@@ -27,7 +31,9 @@ export const DISK_TEXTURES = {
   RED_STRIPES: { value: 'accretion_disk_red_white.png', label: 'Red stripes' },
   RED_PURPLE_STRIPES: { value: 'accretion_disk_red_white_purple.png', label: 'Red-Purple stripes' },
   GRID: { value: 'accretion_disk_grid.png', label: 'Grid' },
+  THERMAL: { value: 'accretion_disk_thermal.png', label: 'Thermal' },
   ARROWS: { value: 'accretion_disk_arrows.png', label: 'Arrows' },
+  CHAOTIC: { value: 'accretion_disk_chaotic.png', label: 'Chaotic' },
   YELLOW: { value: 'accretion_disk_yellow.png', label: 'Yellow' },
   BLUE: { value: 'accretion_disk_blue.png', label: 'Blue' },
 };
@@ -41,6 +47,8 @@ export const DISK_TEXTURE_MAP = {
   [DISK_TEXTURES.ARROWS.value]: diskArrowsUrl,
   [DISK_TEXTURES.YELLOW.value]: diskYellowUrl,
   [DISK_TEXTURES.BLUE.value]: diskBlueUrl,
+  [DISK_TEXTURES.CHAOTIC.value]: diskChaoticUrl,
+  [DISK_TEXTURES.THERMAL.value]: diskThermalUrl,
 };
 
 // Define the available texture options
